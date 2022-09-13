bac6dde5bbc7d9d55e825ce5e9aec94207702b01
Ashik <hajaashik11032002@gmail.com>
2022-09-13 13:36:57 +0530

    adding server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const serverless = require("serverless-http");
+
+
+router.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+
+app.use(`/.netlify/functions/server`, router);
+module.exports.handler = serverless(app);
+
